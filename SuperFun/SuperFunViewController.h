fc6afe2e4849679269238dd9f6c486e3fcb0ec0a
Fiona Van Alstyne <fionava@gmail.com>
2012-11-28 00:33:03 -0800

    Comment updated

--- SuperFun/SuperFunViewController.h
+++ SuperFun/SuperFunViewController.h
@@ -3,8 +3,8 @@
 //  SuperFun
 //
 //  Created by Fiona Van Alstyne on 11/26/12.
-//  Copyright (c) 2012 ItsyBitsy Labs. All rights reserved.
-// 
+//  Copyright (c) 2012 Itsy Bitsy Labs. All rights reserved.
+//
 
 #import <UIKit/UIKit.h>
 
